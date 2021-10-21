f78978f5b72bfa012654381c0b5474b757311475
VladimirBdoyan <vladimirbdoyan1@gmail.com>
2021-10-21 20:15:38 +0300

    Initial Commit

---
+++ git-access-service/src/test/java/com/company/gitaccessservice/GitAccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.gitaccessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitAccessServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
