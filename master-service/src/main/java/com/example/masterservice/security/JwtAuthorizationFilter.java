ed02d52e2724b2e203bc60582a8328ae329564bd
Hayk Aharonyan <hayk.aharonyan.email@gmail.com>
2021-11-22 19:02:40 +0400

    fixed

--- master-service/src/main/java/com/example/masterservice/security/JwtAuthorizationFilter.java
+++ master-service/src/main/java/com/example/masterservice/security/JwtAuthorizationFilter.java
@@ -1,10 +1,13 @@
 package com.example.masterservice.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.masterservice.security.constants.JWTConstants;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,29 +24,30 @@ import java.util.stream.Collectors;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 
-public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 
-
-
-    public JwtAuthorizationFilter(AuthenticationManager authenticationManager) {
+    public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
-
     }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
 
-        String header = request.getHeader(JWTProperties.HEADER_STRING);
-        if (header == null || !header.startsWith(JWTProperties.TOKEN_PREFIX)) {
+        String header = request.getHeader(JWTConstants.HEADER_STRING);
+
+        if (header == null || !header.startsWith(JWTConstants.TOKEN_PREFIX)) {
             chain.doFilter(request, response);
             return;
         }
 
-        String token = header.replace(JWTProperties.TOKEN_PREFIX, "");
+        String token = header.replace(JWTConstants.TOKEN_PREFIX, "");
+
         try {
-            DecodedJWT jwt = JWT.require(HMAC512(JWTProperties.SECRET.getBytes()))
+            DecodedJWT jwt = JWT.require(HMAC512(JWTConstants.SECRET.getBytes()))
                     .build()
                     .verify(token);
+
             String username = jwt.getSubject();
             Claim claim = jwt.getClaim("authorities");
             List<String> authList = claim.asList(String.class);
@@ -51,12 +55,12 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
             Set<GrantedAuthority> authorities = authList.stream()
                     .map(SimpleGrantedAuthority::new)
                     .collect(Collectors.toSet());
-            if (username != null) {
-                AppUser appUser = new AppUser(username, authorities);
-                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
-                        appUser, token, authorities);
-                SecurityContextHolder.getContext().setAuthentication(auth);
-            }
+
+            CurrentUser currentUser = new CurrentUser(username, authorities);
+            Authentication auth = new UsernamePasswordAuthenticationToken(currentUser
+                        , null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+
         } catch (Exception e) {
             SecurityContextHolder.clearContext();
         }
