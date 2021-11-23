e414b8ea53494489a7f5e4f492d586b8dc4338be
Hayk Aharonyan <hayk.aharonyan.email@gmail.com>
2021-11-24 01:19:40 +0400

    minor changes

--- master-service/src/main/java/com/example/masterservice/service/AnalyzerStateTrackerServiceImpl.java
+++ master-service/src/main/java/com/example/masterservice/service/AnalyzerStateTrackerServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.Date;
 import java.util.UUID;
 
 @Service
-@Transactional(propagation = Propagation.REQUIRES_NEW)
+@Transactional
 @RequiredArgsConstructor
 public class AnalyzerStateTrackerServiceImpl implements AnalyzerStateTrackerService {
 
